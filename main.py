58be6934021a621e2d9795eed668e775cd31b7c1
Jarcau Stefan-Cristian <stefan.jarcau@student.unitbv.ro>
2023-11-25 13:50:42 +0200

    Added Token Protection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import uvicorn
 from uuid import uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 import datetime
+import requests
 from dotenv import load_dotenv
 from pod_utils import create_service, create_deployment, create_secret, create_ingress
 from kubernetes import client, config
@@ -35,7 +36,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["localhost"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT"],  # Allows all methods
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
 
@@ -46,117 +47,199 @@ async def connection_test():
 
 
 @app.put("/main_api/create_notebook_instance")
-async def create_notebook_instance(user_id: str, description: str):
-    config.load_incluster_config()
-    apps_v1_api = client.AppsV1Api()
-    core_v1_api = client.CoreV1Api()
-    networking_v1_api = client.NetworkingV1Api()
+async def create_notebook_instance(user_id: str, description: str, authorization: str = Header(None)):
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
 
-    uid = str(uuid4())
-    deployment_body = create_deployment(uid)
-    service_body, service_port = create_service(uid, namespace=namespace)
-    secret_body, password = create_secret(uid)
-    ingress_body = create_ingress(uid, service_port)
-    if service_body is None:
-        return JSONResponse(content="Could not deploy a new instance!", status_code=500)
-    try:
-        core_v1_api.create_namespaced_secret(namespace=namespace, body=secret_body)
-        apps_v1_api.create_namespaced_deployment(namespace=namespace, body=deployment_body)
-        core_v1_api.create_namespaced_service(namespace=namespace, body=service_body)
-        networking_v1_api.create_namespaced_ingress(namespace=namespace, body=ingress_body)
-    except ApiException as e:
-        print(f"Error creating pod {e}")
-        try:
-            networking_v1_api.delete_namespaced_ingress(namespace=namespace, name=f"ingress-{uid}")
-        except ApiException as e:
-            print(f"Resource does not exist! {e}")
+        response = requests.get(os.getenv("KEYCLOAK_URL"), headers={"Authorization": f"Bearer {token}"})
 
-        try:
-            core_v1_api.delete_namespaced_service(namespace=namespace, name=f"service-{uid}")
-        except ApiException as e:
-            print(f"Resource does not exist! {e}")
+        if response.status_code == 200:
+            config.load_incluster_config()
+            apps_v1_api = client.AppsV1Api()
+            core_v1_api = client.CoreV1Api()
+            networking_v1_api = client.NetworkingV1Api()
 
-        try:
-            apps_v1_api.delete_namespaced_deployment(namespace=namespace, name=f"deployment-{uid}")
-        except ApiException as e:
-            print(f"Resource does not exist! {e}")
+            uid = str(uuid4())
+            deployment_body = create_deployment(uid)
+            service_body, service_port = create_service(uid, namespace=namespace)
+            secret_body, password = create_secret(uid)
+            ingress_body = create_ingress(uid, service_port)
+            if service_body is None:
+                return JSONResponse(content="Could not deploy a new instance!", status_code=500)
+            try:
+                core_v1_api.create_namespaced_secret(namespace=namespace, body=secret_body)
+                apps_v1_api.create_namespaced_deployment(namespace=namespace, body=deployment_body)
+                core_v1_api.create_namespaced_service(namespace=namespace, body=service_body)
+                networking_v1_api.create_namespaced_ingress(namespace=namespace, body=ingress_body)
+            except ApiException as e:
+                print(f"Error creating pod {e}")
+                try:
+                    networking_v1_api.delete_namespaced_ingress(namespace=namespace, name=f"ingress-{uid}")
+                except ApiException as e:
+                    print(f"Resource does not exist! {e}")
 
-        try:
-            core_v1_api.delete_namespaced_secret(namespace=namespace, name=f"secret-{uid}")
-        except ApiException as e:
-            print(f"Resource does not exist! {e}")
+                try:
+                    core_v1_api.delete_namespaced_service(namespace=namespace, name=f"service-{uid}")
+                except ApiException as e:
+                    print(f"Resource does not exist! {e}")
 
-        return JSONResponse(content="Could not make notebook!", status_code=500)
+                try:
+                    apps_v1_api.delete_namespaced_deployment(namespace=namespace, name=f"deployment-{uid}")
+                except ApiException as e:
+                    print(f"Resource does not exist! {e}")
 
-    session = Session()
+                try:
+                    core_v1_api.delete_namespaced_secret(namespace=namespace, name=f"secret-{uid}")
+                except ApiException as e:
+                    print(f"Resource does not exist! {e}")
 
-    new_record = MyTable(user_id=user_id, notebook_id=uid, last_accessed=datetime.datetime.now(),
-                         created_at=datetime.datetime.now(), description=description)
-    session.add(new_record)
-    session.commit()
+                return JSONResponse(content="Could not make notebook!", status_code=500)
 
-    session.close()
+            session = Session()
 
-    return_data = {
-        "notebook_id": uid,
-        "notebook_password": password
-    }
+            new_record = MyTable(user_id=user_id, notebook_id=uid, last_accessed=datetime.datetime.now(),
+                                 created_at=datetime.datetime.now(), description=description)
+            session.add(new_record)
+            session.commit()
 
-    return JSONResponse(content=return_data, status_code=201)
+            session.close()
+
+            return_data = {
+                "notebook_id": uid,
+                "notebook_password": password
+            }
+
+            return JSONResponse(content=return_data, status_code=201)
+        else:
+            return JSONResponse(content="Unauthorized access!", status_code=401)
+    else:
+        return JSONResponse(content="Authorization token not provided!", status_code=400)
 
 
 @app.get("/main_api/get_notebook_details")
-async def get_notebook_details(user_id: str):
-    config.load_incluster_config()
+async def get_notebook_details(user_id: str, authorization: str = Header(None)):
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
 
-    apps_v1_api = client.AppsV1Api()
+        response = requests.get(os.getenv("KEYCLOAK_URL"), headers={"Authorization": f"Bearer {token}"})
 
-    session = Session()
+        if response.status_code == 200:
+            config.load_incluster_config()
 
-    instances = session.query(MyTable).filter(MyTable.user_id == user_id).all()
+            apps_v1_api = client.AppsV1Api()
 
-    return_data = []
+            session = Session()
 
-    for instance in instances:
-        notebook_id = instance.notebook_id
-        try:
-            deployment = apps_v1_api.read_namespaced_deployment(f"deployment-{notebook_id}", namespace=namespace)
+            instances = session.query(MyTable).filter(MyTable.user_id == user_id).all()
 
-            creation_timestamp = deployment.metadata.creation_timestamp
-            format_creation_timestamp = creation_timestamp.strftime("%Y:%m:%d %H:%M:%S")
+            return_data = []
 
-            expiration_time = creation_timestamp + datetime.timedelta(days=10)
-            format_expiration_timestamp = expiration_time.strftime("%Y:%m:%d %H:%M:%S")
+            for instance in instances:
+                notebook_id = instance.notebook_id
+                try:
+                    deployment = apps_v1_api.read_namespaced_deployment(f"deployment-{notebook_id}",
+                                                                        namespace=namespace)
 
-        except client.exceptions.ApiException as e:
-            return JSONResponse(content="Exception when calling Kubernetes API: %s\n" % e, status_code=500)
+                    creation_timestamp = deployment.metadata.creation_timestamp
+                    format_creation_timestamp = creation_timestamp.strftime("%Y:%m:%d %H:%M:%S")
 
-        data = {
-            "notebook_id": notebook_id,
-            "creation_time": format_creation_timestamp,
-            "expiration_time": format_expiration_timestamp,
-            "last_accessed": instance.last_accessed,
-            "description": instance.description
-        }
+                    expiration_time = creation_timestamp + datetime.timedelta(days=10)
+                    format_expiration_timestamp = expiration_time.strftime("%Y:%m:%d %H:%M:%S")
 
-        return_data.append(data)
+                except client.exceptions.ApiException as e:
+                    return JSONResponse(content="Exception when calling Kubernetes API: %s\n" % e, status_code=500)
 
-    session.close()
+                data = {
+                    "notebook_id": notebook_id,
+                    "creation_time": format_creation_timestamp,
+                    "expiration_time": format_expiration_timestamp,
+                    "last_accessed": instance.last_accessed,
+                    "description": instance.description
+                }
 
-    return JSONResponse(content=return_data, status_code=200)
+                return_data.append(data)
+
+            session.close()
+
+            return JSONResponse(content=return_data, status_code=200)
+        else:
+            return JSONResponse(content="Unauthorized access!", status_code=401)
+    else:
+        return JSONResponse(content="Authorization token not provided!", status_code=400)
 
 
 @app.post("/main_api/update_access")
-async def update_access(uid: str):
-    session = Session()
+async def update_access(uid: str, authorization: str = Header(None)):
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
 
-    result = session.query(MyTable).filter(MyTable.notebook_id == uid).all()
-    result[0].last_accessed = datetime.datetime.now()
+        response = requests.get(os.getenv("KEYCLOAK_URL"), headers={"Authorization": f"Bearer {token}"})
 
-    session.commit()
-    session.close()
-    return JSONResponse(content="Update Access Successfully!", status_code=200)
+        if response.status_code == 200:
+            session = Session()
 
+            result = session.query(MyTable).filter(MyTable.notebook_id == uid).all()
+            result[0].last_accessed = datetime.datetime.now()
+
+            session.commit()
+            session.close()
+            return JSONResponse(content="Update Access Successfully!", status_code=200)
+        else:
+            return JSONResponse(content="Unauthorized access!", status_code=401)
+    else:
+        return JSONResponse(content="Authorization token not provided!", status_code=400)
+
+
+@app.delete("main_api/delete_notebook")
+async def delete_notebook(uid: str, authorization: str = Header(None)):
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
+
+        response = requests.get(os.getenv("KEYCLOAK_URL"), headers={"Authorization": f"Bearer {token}"})
+
+        if response.status_code == 200:
+            config.load_incluster_config()
+            apps_v1_api = client.AppsV1Api()
+            core_v1_api = client.CoreV1Api()
+            networking_v1_api = client.NetworkingV1Api()
+
+            session = Session()
+
+            row_to_delete = session.query(MyTable).filter(MyTable.notebook_id == uid).first()
+
+            if row_to_delete:
+                session.delete(row_to_delete)
+                session.commit()
+            else:
+                return JSONResponse(content="Failed to delete notebook!", status_code=500)
+
+            session.close()
+
+            try:
+                networking_v1_api.delete_namespaced_ingress(namespace=namespace, name=f"ingress-{uid}")
+            except ApiException as e:
+                print(f"Resource does not exist! {e}")
+
+            try:
+                core_v1_api.delete_namespaced_service(namespace=namespace, name=f"service-{uid}")
+            except ApiException as e:
+                print(f"Resource does not exist! {e}")
+
+            try:
+                apps_v1_api.delete_namespaced_deployment(namespace=namespace, name=f"deployment-{uid}")
+            except ApiException as e:
+                print(f"Resource does not exist! {e}")
+
+            try:
+                core_v1_api.delete_namespaced_secret(namespace=namespace, name=f"secret-{uid}")
+            except ApiException as e:
+                print(f"Resource does not exist! {e}")
+
+            return JSONResponse(content="Deleted Notebook Successfully!", status_code=200)
+        else:
+            return JSONResponse(content="Unauthorized access!", status_code=401)
+    else:
+        return JSONResponse(content="Authorization token not provided!", status_code=400)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0')
